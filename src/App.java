30a83195ac7f431507f505a6f196552f27c2fca7
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-11-05 19:22:43 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numeroIntroducido = Integer.parseInt(System.console().readLine("Por favor, introduzca un número entero positivo: "));
+
+        int numero = numeroIntroducido;
+        int volteado = 0;
+        int longitud = 0;
+
+        while (numero > 0) {
+            volteado = (volteado * 10) + (numero % 10);
+            numero /= 10;
+            longitud++;
+        }
+
+        int cifra = 0;
+
+        for (int i = 0; i < longitud; i++) {
+            cifra = volteado % 10;
+            volteado /= 10;
+            for (int j = 0; j < cifra; j++) {
+                System.out.print("|");
+            }
+            if (i < longitud-1)
+                System.out.print("-");
+        }
+
+        
     }
 }
